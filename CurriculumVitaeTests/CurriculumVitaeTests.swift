53eaf363bc872454bc9f1d5f3346733955bb14a5
Michael Voong <michael@voo.ng>
2021-03-20 14:22:44 +0000

    Fastlane

---
+++ CurriculumVitaeTests/CurriculumVitaeTests.swift
@@ -0,0 +1,33 @@
+//
+//  CurriculumVitaeTests.swift
+//  CurriculumVitaeTests
+//
+//  Created by Michael Voong on 20/03/2021.
+//  Copyright © 2021 Meidi Limited. All rights reserved.
+//
+
+import XCTest
+
+class CurriculumVitaeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
